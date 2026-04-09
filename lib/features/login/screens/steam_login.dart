734ad6eba2cdbdfb4ef2db7ad603ecae79581383
noah <noahfoley6@gmail.com>
2026-04-09 13:50:31 -0500

    updates repo for new flutter version

--- lib/features/login/screens/steam_login.dart
+++ lib/features/login/screens/steam_login.dart
@@ -1,31 +1,47 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:steam_achievement_tracker/services/widgets/my_app_bar.dart';
 import 'package:steam_login/steam_login.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class SteamLogin extends StatelessWidget {
-  final _webView = FlutterWebviewPlugin();
-
+class SteamLogin extends StatefulWidget {
   SteamLogin({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Listen to the onUrlChanged events, and when we are ready to validate do so.
-    _webView.onUrlChanged.listen((String url) async {
-      var openId = OpenId.fromUri(Uri.parse(url));
-      if (openId.mode == 'id_res') {
-        await _webView.close();
-        // ignore: use_build_context_synchronously
-        Navigator.of(context).pop(openId.validate());
-      }
-    });
+  State<SteamLogin> createState() => _SteamLoginState();
+}
 
-    var openId = OpenId.raw('https://steamer', 'https://steamer/', {});
-    return WebviewScaffold(
-      url: openId.authUrl().toString(),
+class _SteamLoginState extends State<SteamLogin> {
+  late final WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    final openId = OpenId.raw('https://steamer', 'https://steamer/', {});
+    _webViewController =
+        WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onNavigationRequest: (request) {
+                final openId = OpenId.fromUri(Uri.parse(request.url));
+                if (openId.mode == 'id_res') {
+                  Navigator.of(context).pop(openId.validate());
+                  return NavigationDecision.prevent;
+                }
+                return NavigationDecision.navigate;
+              },
+            ),
+          )
+          ..loadRequest(openId.authUrl());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: myAppBar(title: 'STEAMER'),
+      body: WebViewWidget(controller: _webViewController),
     );
   }
 }
